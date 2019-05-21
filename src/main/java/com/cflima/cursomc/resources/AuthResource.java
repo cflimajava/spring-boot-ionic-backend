8499fdd2103b9277b8ce9f4656a53ef3ff002a41
cflimajava <criferlim@yahoo.com.br>
2019-05-21 17:22:49 +0100

    Refresh Token

--- src/main/java/com/cflima/cursomc/resources/AuthResource.java
+++ src/main/java/com/cflima/cursomc/resources/AuthResource.java
@@ -2,16 +2,28 @@ package com.cflima.cursomc.resources;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cflima.cursomc.security.JWTUtil;
+import com.cflima.cursomc.security.UserSS;
+import com.cflima.cursomc.serivices.UserService;
+
 @RestController
-@RequestMapping(value="/auth")
+@RequestMapping(value = "/auth")
 public class AuthResource {
 
-	
-	public ResponseEntity<Void> refreshToken(HttpServletResponse response){
-		return null;
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		return ResponseEntity.noContent().build();
 	}
 }
